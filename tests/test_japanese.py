a537640bd40a74c0cfb16cad9d78015dad8abc02
finia2na <undifini@gmail.com>
2021-03-01 13:31:26 +0100

    optimize the test

--- tests/test_japanese.py
+++ tests/test_japanese.py
@@ -1,6 +1,6 @@
 def test_japanese_punctuation():
   from pygoogletranslation import Translator
   t = Translator()
-  assert t.translate("今日は。今晩は").text == "Today. Tonight"
-  assert t.translate("今日は、今晩は").text == "Today, tonight"
+  assert t.translate("おはよう。げんきですか。").text == "Good morning. How are you." #<--- this is how google translate web translates it. Sadly, with the english-point replacement method, the result is different from this, as can be seen in the test.
+  assert t.translate("今日、明日").text == "Today, tomorrow"
   # assert t.translate("「こんにちは」").text == "\"Hello\""
